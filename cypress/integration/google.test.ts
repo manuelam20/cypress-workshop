98becea26db200efe733f1ad4845250d18ea45ab
Manuela Molina <manuela.molina@teaminternational.com>
2019-06-18 10:20:18 -0500

    micha report

--- cypress/integration/google.test.ts
+++ cypress/integration/google.test.ts
@@ -1,6 +1,13 @@
 describe('This is the first example of cypress', () => {
   it('should have a title', () => {
+    cy.server();
+    cy.fixture('google-response').as('googleSearchResponse');
+    cy.route('GET', '**/complete/search**', '@googleSearchResponse');
+
     cy.visit('https://www.google.com/');
+    cy.get('[name="q"]')
+      .type('Cypress');
+
     cy.title().should('be.equal', 'Google');
   });
 });
